560ef73959f4ae6a321a508f36ee8d20923af4c5
Jason McGinty <jason_mcginty@yahoo.com>
2020-09-22 18:53:17 -0500

    updating environment variables in app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, render_template, redirect, flash, jsonify, ses
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User, Feedback
 from forms import RegistrationForm, LoginForm, FeedbackSubmissionForm
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yeet'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'yeet')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback_app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
