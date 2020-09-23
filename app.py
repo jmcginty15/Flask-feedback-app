b1305bd9b9fa5095da3c7b009cb15adb5ed68383
Jason McGinty <jason_mcginty@yahoo.com>
2020-09-22 19:13:40 -0500

    updating database url

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'yeet')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///feedback_app')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
